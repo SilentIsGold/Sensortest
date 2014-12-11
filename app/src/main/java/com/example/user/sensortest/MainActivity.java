4cb585b1dcfbbcb4db793e9a7002fc0ba6cb1e0a
silent <a23078911@gmail.com>
2014-12-11 23:06:01 +0800

    layout介面更改  spinner現在用horizontal來當底 sensorlist會把sesor印出來  還不會顯示資料

--- app/src/main/java/com/example/user/sensortest/MainActivity.java
+++ app/src/main/java/com/example/user/sensortest/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.user.sensortest;
 
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
@@ -14,6 +16,7 @@ import android.widget.Spinner;
 import android.widget.TextView;
 
 import java.util.Arrays;
+import java.util.List;
 
 import static android.view.View.VISIBLE;
 
@@ -27,11 +30,12 @@ public class MainActivity extends ActionBarActivity {
     private Spinner SpinnerPhone1;
     private Spinner SpinnerPhone2;
     private Spinner SpinnerMotion;
+    private SensorManager m_sensorManager;
 
 
     private String[] FileList = new String[3];
     private String[] PhoneList = {"SonyTx","HTCSensation","SamsungGTS681","SonyXS","ASUSPadefone","SonyT3"};
-    private String[] MotionList = {"上下","左右","X","手臂自然揮動"};
+    private String[] MotionList = {"垂直上下","水平左右","X","自然上下揮動"};
 
 
     @Override
@@ -40,11 +44,53 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
         InitValue();
         InitSpinner();
+        ShowSensor();
         Log.i("APPTEST", "is open");
 
 
 
 
+    }
+
+    public void ShowSensor(){
+        List<Sensor> availableSensor = m_sensorManager.getSensorList(Sensor.TYPE_ALL);//取得手機感測器清單
+        String outputString = "";
+
+        for (int i =0; i<availableSensor.size(); i++  ){
+            Sensor thisSensor = availableSensor.get(i);
+
+            outputString  += thisSensor.getName(); //取得裝置名稱
+            outputString  += PrintSensorType(thisSensor.getType());  //取得型態
+            outputString  += "\n";
+            /*
+            //outputString  += ", ";
+
+            outputString  += String.valueOf(thisSensor.getPower()); //取得功耗
+            outputString  += "毫安培\n";
+            outputString  += thisSensor.getVendor(); //取得供應商
+            outputString  += String.valueOf(thisSensor.getMaximumRange()); //資料範圍
+            outputString  += "\n";
+            */
+        }
+        SensorListText.setText(outputString);
+    }
+    private String PrintSensorType(int i){
+        switch(i){
+            case 1: return "(三軸加速度感應器)";
+            case 2: return "(磁力感應器)";
+            case 3: return "(方向感應器)";
+            case 4: return "(陀螺儀感應器)";
+            case 5: return "(光感應器)";
+            case 6: return "(壓力感應器)";
+            case 7: return "(溫度感應器)";
+            case 8: return "(遠近感應器)";
+            case 9: return "(重力感測器)";
+            case 10: return "(線性加速度感應器)";
+            case 11: return "(翻轉感測器)";
+
+            default: return String.valueOf(i);
+        }//switch
+        //return "";
     }
 
     public void InitValue(){
@@ -62,6 +108,8 @@ public class MainActivity extends ActionBarActivity {
         FileList[1]=PhoneList[1];
         FileList[2]=MotionList[2];
 
+
+        m_sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);//取得手機的感測器資訊
     }
 
     public void InitSpinner(){
